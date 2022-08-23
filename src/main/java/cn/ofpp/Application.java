701901b16d42af03b9dbcccb8dec80a15ab48daa
16696527628 <103370534+16696527628@users.noreply.github.com>
2022-08-24 00:27:50 +0800

    Update Application.java

--- src/main/java/cn/ofpp/Application.java
+++ src/main/java/cn/ofpp/Application.java
@@ -27,14 +27,12 @@ public class Application {
 
         // -----------------  以下为演示数据  ------------------------
 
-        // new 一个 女友
-        GirlFriend girlFriend = new GirlFriend("某女友",
-                "江苏省", "南京市", "1999-08-08", "2011-04-16", "oQFk-5qtXv2uGNCu9oiCiV85KWD8");
+        GirlFriend girlFriend = new GirlFriend("张紫阳宝宝",
+                "河南省", "新乡市", "2003-10-23", "2022-08-04", "oD1gX6ABTlHOX5lHuYyplfIUYk-o");
         Wx.sendTemplateMessage(MessageFactory.resolveMessage(girlFriend));
 
-        // new 一个 男友 也可单独针对一个friend设置模板ID 以达到不同人不同消息
-        BoyFriend boyFriend = new BoyFriend("某男友",
-                "江苏省", "南京市", "1999-08-08", "2011-04-16", "oQFk-5qtXv2uGNCu9oiCiV85KWD8", "5t7-Ksy8_rw-QmUkxf8J7Pe-QLQ2rBc7RWJi_pSmeh4");
+        BoyFriend boyFriend = new BoyFriend("李明渊宝宝",
+                "河南省", "郑州市", "2002-08-22", "2022-08-04", "oD1gX6PI0A_95cEWll49yCP4EaRQ");
         Wx.sendTemplateMessage(MessageFactory.resolveMessage(boyFriend));
     }
 
